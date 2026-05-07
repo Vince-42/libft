add07a18c0fb50fc6b1eb5b03512cd82f351a1a9
Vincent <vleroy@student.42berlin.de>
2026-05-07 10:55:44 +0200

    memmove

--- libft.h
+++ libft.h
@@ -13,5 +13,6 @@ int     ft_isprint(int c);
 void    *ft_memset(void *b, int c, size_t len);
 void    ft_bzero(void *s, size_t n);
 void    *ft_memcpy(void *restrict dst, const void *restrict src, size_t n);
+void    *ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
