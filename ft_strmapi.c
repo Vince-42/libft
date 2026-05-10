165786a2b13babd34fa60b117c737064938cc276
Vincent <vleroy@student.42berlin.de>
2026-05-11 01:02:53 +0200

    Applies the function f to each character of the string s, passing its index as the first argument and the character itself as the second. A new string is created (using malloc(3)) to store the results from the successive applications of f.

--- ft_strmapi.c
+++ ft_strmapi.c
@@ -10,4 +10,44 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdlib.h>
+/*
+Parameters
+s: The string to iterate over.
+f: The function to apply to each character.
 
+Returns 
+    The string created from the successive applications
+    of ’f’.
+Returns 
+    NULL if the allocation fails.
+
+Description
+Applies the function f to each character of the
+string s, passing its index as the first argument
+and the character itself as the second. A new
+string is created (using malloc(3)) to store the
+results from the successive applications of f.
+*/
+
+char    *ft_strmapi(char const *s, char (*f)(unsigned int, char));
+
+char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+    unsigned int    str_size;
+    unsigned int    i;
+    char            *new_str;
+
+    i = 0;
+    str_size = ft_strlen(s);
+    new_str = malloc(sizeof(char) * str_size + 1);
+    if (!new_str)
+        return (NULL);
+    while(i < str_size)
+    {
+        new_str[i] = f(i, s[i]);
+        i++;
+    }
+    return (new_str);
+}
