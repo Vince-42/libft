4d91cfd2bfb5bb12154c9b725af33c77810d431f
Vincent <vleroy@student.42berlin.de>
2026-05-11 01:02:01 +0200

    Applies the function ’f’ to each character of the     string passed as argument, passing its index as     the first argument. Each character is passed by     address to ’f’ so it can be modified if necessary.

--- ft_striteri.c
+++ ft_striteri.c
@@ -10,4 +10,35 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+/*
+s: The string to iterate over.
+f: The function to apply to each character.
+Return 
+    Value None
+Description 
+    Applies the function ’f’ to each character of the
+    string passed as argument, passing its index as
+    the first argument. Each character is passed by
+    address to ’f’ so it can be modified if necessary.
+*/
 
+#include "libft.h"
+
+void ft_striteri(char *s, void (*f)(unsigned int, char*));
+
+void ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+    unsigned int    str_totalsize;
+    char            *new_str;
+    unsigned int    i;
+
+    i = 0;
+    str_totalsize = ft_strlen(s);
+    new_str = ft_calloc(str_totalsize + 1, sizeof(char));
+    while (i < str_totalsize)
+    {
+        f(i,&s[i]);
+        new_str[i] = s[i];
+        i++;
+    }
+}
