d94fa31ee211da8a2235cef59cfebf8cbc7defbe
Vincent <vleroy@student.42berlin.de>
2026-05-11 01:39:33 +0200

    write to fd

--- ft_putchar_fd.c
+++ ft_putchar_fd.c
@@ -10,4 +10,12 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <unistd.h>
 
+void    ft_putchar_fd(char c, int fd);
+
+void    ft_putchar_fd(char c, int fd)
+{
+    write(fd,&c,1);
+}
--- ft_putstr_fd.c
+++ ft_putstr_fd.c
@@ -10,4 +10,19 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <unistd.h>
 
+void ft_putstr_fd(char *s, int fd);
+
+void ft_putstr_fd(char *s, int fd)
+{
+    int i;
+
+    i = 0;
+    while (s[i])
+    {
+        write(fd, &s[i],1);
+        i++;
+    }
+}
