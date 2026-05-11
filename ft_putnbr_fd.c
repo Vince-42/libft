55f19d828c6ba3a9877927c7aa325c5bc46cbda9
Vincent <vleroy@student.42berlin.de>
2026-05-11 13:38:25 +0200

    ft_putnbr_fd ok

--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -10,4 +10,33 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <unistd.h>
 
+void    ft_putnbr_fd(int n, int fd);
+
+void    ft_putnbr_fd(int n, int fd)
+{
+    char res_digit;
+
+    if (n == -2147483648)
+    {
+        write(fd, "-2147483648", 11);
+        return ;
+    }
+    else if (n < 0)
+    {
+        write(fd, "-", 1);
+        n = n * -1;
+    }
+    if (n > 9)
+    {
+        ft_putnbr_fd(n / 10, fd);
+        ft_putnbr_fd(n % 10, fd);
+    }
+    else
+    {
+        res_digit = n + '0';
+        write(fd, &res_digit, 1);
+    }
+}
