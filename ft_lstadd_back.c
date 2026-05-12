3cd8d6e91e473c96e64ec0aed7ee02e1fe93c270
Vincent <vleroy@student.42berlin.de>
2026-05-12 11:06:55 +0200

    add front + header | add back full function

--- ft_lstadd_back.c
+++ ft_lstadd_back.c
@@ -10,4 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+void	ft_lstadd_back(t_list **lst, t_list *new);
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	while (*lst)
+	{
+		lst = &((*lst)->next);
+	}
+	*lst = new;
+}
