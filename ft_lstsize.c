40a96d57b2d4760f0a3d90ef85ca345e63357b57
Vincent <vleroy@student.42berlin.de>
2026-05-12 11:07:53 +0200

    full working function

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -10,4 +10,20 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+#include <stdio.h>
 
+int	ft_lstsize(t_list *lst);
+
+int	ft_lstsize(t_list *lst)
+{
+	int	count;
+
+	count = 0;
+	while (lst != NULL)
+	{
+		count++;
+		lst = lst->next;
+	}
+	return (count);
+}
